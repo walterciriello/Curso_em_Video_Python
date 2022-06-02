f9447081180871ffc7002f23768e6c61a7d879ed
Walter R Ciriello <106283495+walterciriello@users.noreply.github.com>
2022-06-02 19:46:27 -0300

    update

--- Exercícios/exercicios_python_curso_em_video (22).py
+++ Exercícios/exercicios_python_curso_em_video (22).py
@@ -1,4 +1,11 @@
-high = float(input('How high is the wall in meters? '))
-wide = float(input('How wide is the wall in meters? '))
-print(f'The total area the wall is \033[34m{high * wide:.2f}\033[m square meters.')
-print(f'You will need \033[34m{high * wide / 2:.2f}\033[m cans of paint.')
+# Exercise number 22
+
+high = float(input("How high is the wall in meters?\n"))
+wide = float(input("How wide is the wall in meters?\n"))
+
+total_area = high * wide
+cans_of_paint = high * wide / 2
+
+print(f"The total area the wall is \033[34m{total_area:.2f}\033[m square meters.")
+
+print(f"You will need \033[34m{cans_of_paint:.2f}\033[m cans of paint.")
--- Exercícios/exercicios_python_curso_em_video (25).py
+++ Exercícios/exercicios_python_curso_em_video (25).py
@@ -1,8 +1,7 @@
-print('-'*40)
-print('Exercício nº 011')
-print('-'*40)
-sal = float(input('Digite o salário do funcionário: R$ '))
-nsal = sal + (sal*15/100)
-print('O salário do funcionário após os 15% de aumento será de R$ {:.2f}'.format(nsal))
-print('-'*40)
+# Exercise number 25
 
+salario = float(input("Digite o salário do funcionário: R$ "))
+aumento = 0
+aumento += salario * 15 / 100
+novo_salario = salario + aumento
+print(f"O salário do funcionário após os 15% de aumento será de R${novo_salario:.2f}")
